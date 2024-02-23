851f0637566bf457725ec10f6beba7d91ca06a1e
Mitch Dzugan <mitchdzugan@gmail.com>
2024-02-23 11:44:45 -0600

    checkin

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-import("./lib/app.js").then(({ exec }) => exec(process.argv));
+// import("./dist/index.js").then(({ exec }) => exec(process.argv));
+nw.Window.open('index.html', {}, function(win) {
+    console.log(win);
+  });
